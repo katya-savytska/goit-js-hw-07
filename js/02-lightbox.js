e5b144926025bfd652b15328f273ce21923eb679
Савицька Катерина <savytskaa.kate@gmail.com>
2023-01-16 15:20:40 +0300

    Task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createCard(galleryItems);
+
+function createCard(galleryItems){
+    return galleryItems
+    .map(({preview, original, description})=>{
+        return `<li><a class='gallery__link' href='${original}'>
+        <img  class='gallery__image' src='${preview}' alt='${description}'>
+        </a></li>`;})
+        .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== `IMG`){
+        return;
+    }
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+
+});
